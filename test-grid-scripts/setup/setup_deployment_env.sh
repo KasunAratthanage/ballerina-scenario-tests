d66d02eab20794f5fc8b9605baf8c2b2b81c491d
KasunAratthanage <kasunaratthanage@gmail.com>
2019-07-19 15:09:52 +0530

    Update sh

--- test-grid-scripts/setup/setup_deployment_env.sh
+++ test-grid-scripts/setup/setup_deployment_env.sh
@@ -32,9 +32,9 @@ setup_deployment_env() {
     echo "======Starting++++++++++++++++++++++++++++++++++++"
     pwd
     ls
-    echo "====== infra location : ${input_dir}"
+    echo "====== infra location : ${INPUT_DIR}"
     cat infrastructure.properties
-    read_property_file "${input_dir}/infrastructure.properties" infra_config
+    read_property_file "${INPUT_DIR}/infrastructure.properties" infra_config
     
 
     readonly docker_user=${infra_config["dockerhub_ballerina_scenarios_username"]}
